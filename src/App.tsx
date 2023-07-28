479efb1c67cf278050b1b47dcfac82d6ec54acd5
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-28 20:18:02 -0300

    clear projetct

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,10 @@ import { Provider as ReduxProvider } from "react-redux"
 
 import { store } from "./store"
 
-import { AddTodo } from "./components/AddTodo"
-import { TodoList } from "./components/TodoList"
-
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
+      <h1>Hello</h1>
     </ReduxProvider>
   )
 }
