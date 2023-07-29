43dca8a5a51bbaa7512d2ac6a0dbe567456c8379
erik-ferreira <erikdesouzagf@gmail.com>
2023-07-29 12:28:34 -0300

    init structure page; add scrollbar plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import "./styles/global.css"
+
 import { Provider as ReduxProvider } from "react-redux"
 
 import { store } from "./store"
+import { Player } from "./pages/Player"
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <h1>Hello</h1>
+      <Player />
     </ReduxProvider>
   )
 }
