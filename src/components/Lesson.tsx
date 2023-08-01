37e25b71417cb2da204be800df85cdc09a058eb4
erik-ferreira <erikdesouzagf@gmail.com>
2023-08-01 10:52:23 -0300

    feat: styling lesson active

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,18 +1,30 @@
-import { VideoCamera } from "phosphor-react"
+import { VideoCamera, PlayCircle } from "phosphor-react"
 
 interface LessonProps {
   title: string
   duration: string
+  isCurrent?: boolean
   onPlay: () => void
 }
 
-export function Lesson({ title, duration, onPlay }: LessonProps) {
+export function Lesson({
+  title,
+  isCurrent = false,
+  duration,
+  onPlay,
+}: LessonProps) {
   return (
     <button
-      className="flex items-center gap-3 text-sm text-zinc-400"
       onClick={onPlay}
+      data-active={isCurrent}
+      disabled={isCurrent}
+      className="flex items-center gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-500 data-[active=true]:font-bold enabled:hover:text-zinc-100"
     >
-      <VideoCamera className="w-4 h-4 text-zinc-500" />
+      {isCurrent ? (
+        <PlayCircle className="w-4 h-4 text-emerald-500" />
+      ) : (
+        <VideoCamera className="w-4 h-4 text-zinc-500" />
+      )}
       <span>{title}</span>
       <span className="ml-auto font-mono text-xs text-zinc-500">
         {duration}
